0bea5e3c384a8bf819f0818de9611a8c26bc5e68
Amit Saxena <48174393+amitsaxena098@users.noreply.github.com>
2026-03-21 23:57:57 +0530

    Initial commit (#1)

---
+++ src/main/java/com/amitsaxena098/rlaas_users_service/RlaasUsersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.amitsaxena098.rlaas_users_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RlaasUsersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RlaasUsersServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amitsaxena098/rlaas_users_service/RlaasUsersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amitsaxena098.rlaas_users_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RlaasUsersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
